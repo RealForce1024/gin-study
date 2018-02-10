c03930c1e7fe2b11ebc2e471d9d7d1d047f94890
fqc <fengqichao171106@credithc.com>
2018-02-10 16:12:06 +0800

    add net/http to use http.StatusOK

--- 001-hello-gin.go
+++ 001-hello-gin.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,7 +10,7 @@ func main() {
 	// gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
